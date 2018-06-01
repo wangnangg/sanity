2896dced5e4838d0fc708f1e3ab9939b00696720
Nan Wang <wangnangg@gmail.com>
2018-05-31 21:10:25 -0400

    exp

--- experiment/power_grid_model/grad_ieee14.cpp
+++ experiment/power_grid_model/grad_ieee14.cpp
@@ -34,7 +34,7 @@ TEST(power_grid_model, ieee14_grad)
     };
 
     uint base = trunc.size() - 1;
-    auto org_res = solveDiff(context, trunc[base]);
+    Real org_reward = 2.48054;
     std::vector<DiffTrunc> candi;
 
     if (trunc[base].bus < context.model.nbus)
@@ -66,7 +66,7 @@ TEST(power_grid_model, ieee14_grad)
     for (uint i = 0; i < candi.size(); i++)
     {
         auto new_res = solveDiff(context, candi[i]);
-        Real gain = std::abs(org_res.reward - new_res.reward);
+        Real gain = std::abs(org_reward - new_res.reward);
         if (gain > max)
         {
             max = gain;
